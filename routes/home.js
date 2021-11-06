7b4a4e624d397ccfc6aef31b72349a68fc594d2b
Lochan Melam <lochan99melam@gmail.com>
2021-11-06 17:04:31 +0530

    display products in home page

--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const router = express.Router();
-
+const request = require("request");
 function loginCheck(req, res, next) {
   req.session.loggedIn ? next() : res.redirect("/signin");
 }
 
 router.get("/", loginCheck, (req, res) => {
-  res.render("home", { user: req.session.user });
+  request(
+    `http://${req.get("host")}/api/allProducts?home=${true}`,
+    { json: true },
+    (error, response, body) => {
+      var result = [[], [], [], []];
+      for (var i = 0; i < body.length; i++) {
+        if (body[i].category == "M") result[0].push(body[i]);
+        else if (body[i].category == "L") result[1].push(body[i]);
+        else if (body[i].category == "H") result[2].push(body[i]);
+        else result[3].push(body[i]);
+      }
+      res.render("home", { user: req.session.user, result: result });
+    }
+  );
 });
 
 module.exports = router;
--- routes/searchEngine.js
+++ routes/searchEngine.js
@@ -2,11 +2,11 @@ const express = require("express");
 var router = express.Router();
 const db = require("../configuration/dbConnection");
 router.get("/", (req, res) => {
-  var get_list =
-    "SELECT * FROM Products WHERE name LIKE '" + req.query.search + "%" + "'";
-  if (req.query.category) {
-    get_list =
-      "SELECT * FROM Products WHERE category = '" + req.query.category + "'";
+  var get_list = req.query.search
+    ? "SELECT * FROM Products WHERE name LIKE '" + req.query.search + "%" + "'"
+    : "SELECT * FROM Products WHERE category = '" + req.query.category + "'";
+  if (req.query.home) {
+    get_list = "SELECT * FROM Products";
   }
   db.query(get_list, (err, response) => {
     let resultArray = Object.values(JSON.parse(JSON.stringify(response)));
